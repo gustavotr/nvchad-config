20e2c476886d507938aa4f4cffe44bd86131ee71
Gustavo T. Rudiger <gustavo@trudax.tech>
2023-05-17 00:46:44 -0300

    feat: 🎸 add async format on save

--- configs/null-ls.lua
+++ configs/null-ls.lua
@@ -4,30 +4,57 @@ if not present then
   return
 end
 
-local b = null_ls.builtins
+local formatting = null_ls.builtins.formatting
+local diagnostics = null_ls.builtins.diagnostics
+local code_actions = null_ls.builtins.code_actions
 
 local sources = {
 
   -- webdev stuff
-  b.formatting.deno_fmt, -- chose deno for ts/js files cuz its very fast!
-  b.formatting.prettier,
+  formatting.deno_fmt, -- chose deno for ts/js files cuz its very fast!
+  formatting.prettier,
 
-  b.formatting.eslint,
-  b.code_actions.eslint,
-  b.diagnostics.eslint,
+  formatting.eslint,
+  code_actions.eslint,
+  diagnostics.eslint,
 
   -- Lua
-  b.formatting.stylua,
+  formatting.stylua,
 
   -- cpp
-  b.formatting.clang_format,
+  formatting.clang_format,
 
-  b.diagnostics.codespell,
-
-  b.diagnostics.markdownlint,
-  b.diagnostics.tsc,
-  b.diagnostics.gospel,
+  diagnostics.codespell,
+  diagnostics.markdownlint,
+  diagnostics.tsc,
+  diagnostics.gospel,
 }
+
+local async_formatting = function(bufnr)
+  bufnr = bufnr or vim.api.nvim_get_current_buf()
+
+  vim.lsp.buf_request(bufnr, "textDocument/formatting", vim.lsp.util.make_formatting_params {}, function(err, res, ctx)
+    if err then
+      local err_msg = type(err) == "string" and err or err.message
+      -- you can modify the log message / level (or ignore it completely)
+      vim.notify("formatting: " .. err_msg, vim.log.levels.WARN)
+      return
+    end
+
+    -- don't apply results if buffer is unloaded or has been modified
+    if not vim.api.nvim_buf_is_loaded(bufnr) or vim.api.nvim_buf_get_option(bufnr, "modified") then
+      return
+    end
+
+    if res then
+      local client = vim.lsp.get_client_by_id(ctx.client_id)
+      vim.lsp.util.apply_text_edits(res, bufnr, client and client.offset_encoding or "utf-16")
+      vim.api.nvim_buf_call(bufnr, function()
+        vim.cmd "silent noautocmd update"
+      end)
+    end
+  end)
+end
 
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
@@ -38,8 +65,7 @@ local on_attach = function(client, bufnr)
       group = augroup,
       buffer = bufnr,
       callback = function()
-        -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-        vim.lsp.buf.format { bufnr = bufnr }
+        async_formatting(bufnr)
       end,
     })
   end
